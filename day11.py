d064d479b11a2605943587558a52a43caf4d0f92
Genia Boeing <genia@gboeing.com>
2022-02-03 22:22:28 +0100

    Correct plural of octopuses

--- day11.py
+++ day11.py
@@ -3,9 +3,9 @@ input_file = 'input11'
 with open(input_file) as f:
     lines = f.readlines()
 
-octopussies = [0]*len(lines)
+octopuses = [0]*len(lines)
 for i in range(0, len(lines)):
-    octopussies[i] =[int(l) for l in  lines[i].strip()]
+    octopuses[i] =[int(l) for l in  lines[i].strip()]
 
 
 def propagate_light(board, y, x):
@@ -23,34 +23,34 @@ def propagate_light(board, y, x):
                 
 
 max_steps = 500
-max_x = len(octopussies[0])
-max_y = len(octopussies)
+max_x = len(octopuses[0])
+max_y = len(octopuses)
 flash_counter = 0
 for step in range(0, max_steps):
     #add 1 to all the values
     for i in range(0, max_y):
         for j in range(0, max_x):
-            octopussies[i][j] += 1
+            octopuses[i][j] += 1
 
 
     #make it flash 
     activated_octos = []
     for i in range(0, max_y):
         for j in range(0, max_x):
-            if octopussies[i][j] == 10:
+            if octopuses[i][j] == 10:
                 activated_octos.append([i,j])
 
     for i in range(0, len(activated_octos)):
-        octopussies = propagate_light(octopussies, activated_octos[i][0], activated_octos[i][1])
+        octopuses = propagate_light(octopussies, activated_octos[i][0], activated_octos[i][1])
 
     flashes_step = 0
     #count flashes and set to 0 
     for i in range(0, max_y):
         for j in range(0, max_x):
-            if octopussies[i][j] > 9:
+            if octopuses[i][j] > 9:
                 flashes_step += 1
                 flash_counter += 1
-                octopussies[i][j] = 0
+                octopuses[i][j] = 0
     print('step ', step, 'flash_counter ', flash_counter)
     if flashes_step == max_x * max_y:
         print('step ', step + 1, ' all the octos are flashing')
